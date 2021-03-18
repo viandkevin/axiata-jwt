345bcfac5fb4cab3cb8a0df077d05c66cfd38840
viandkevin <54173323+viandkevin@users.noreply.github.com>
2021-03-19 00:51:51 +0700

    add security endpoint

--- src/main/java/com/axiata/jwt/config/WebSecurityConfig.java
+++ src/main/java/com/axiata/jwt/config/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/authenticate", "/register", "/changePassword")
+		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/authenticate", "/register", "/changePassword", "/findByUsername", "/findByEmployeeNumber", "/pageUser", "/editProfile")
 				.permitAll().anyRequest().authenticated().and().exceptionHandling()
 				.authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
